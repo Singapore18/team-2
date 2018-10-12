7871399d17e78cc86a0ba93094d4c67a4d4a1f56
Joey Cxt <joey.chan.2016@smu.edu.sg>
2018-10-12 12:23:09 +0800

    Add textfield

--- my-app/src/components/Forms/AddJob.js
+++ my-app/src/components/Forms/AddJob.js
@@ -4,49 +4,107 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const styles = (theme) => ({
     container: {
-      display: 'flex',
-      flexWrap: 'wrap',
+        display: 'flex',
+        flexWrap: 'wrap',
     },
     textField: {
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit,
         width: 200,
-      },
+    },
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 2,
+        marginRight: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 3,
+        marginBottom: theme.spacing.unit * 3,
+        padding: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+            marginTop: theme.spacing.unit * 6,
+            marginBottom: theme.spacing.unit * 6,
+            padding: theme.spacing.unit * 3,
+        },
+    }
 });
 
 class AddJob extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            name: "Company Name",
+            name: "Enter your company name",
         }
     }
 
     handleChange = name => event => {
         this.setState({
-          [name]: event.target.value,
+            [name]: event.target.value,
         });
-      };
+    };
 
     render() {
 
-        const {classes} = this.props;
-        return(
-            <div className="col-md-4 col-md-offset-4">
-                <form className={classes.container} noValidate autoComplete="off">
-                    <TextField
-                    id="standard-name"
-                    label="Name"
-                    className={classes.textField}
-                    value={this.state.name}
-                    onChange={this.handleChange('name')}
-                    margin="normal"
-                    />
-                </form>
-            </div>
+        const { classes } = this.props;
+        return (
+            <div className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography variant="h6" gutterBottom>
+                        Add Job
+                    </Typography>
+                    <form className={classes.container} noValidate autoComplete="off">
+                        <Grid container spacing={24}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="company_name"
+                                name="company_name"
+                                label="Company Name"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="address"
+                                name="address"
+                                label="Address"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="region"
+                                name="region"
+                                label="Region"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="region"
+                                name="region"
+                                label="Region"
+                                fullWidth
+                            />
+                        </Grid>
+                        </Grid>
+                    </form>
+                </Paper>
+            </div >
         );
     }
 }
