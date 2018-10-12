c7d3a859ece181dc68e7e4b2131cf8d937d500cf
Joey Cxt <joey.chan.2016@smu.edu.sg>
2018-10-12 14:35:14 +0800

    resume template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import AppBar from './components/Navigation/AppBar';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import AddJob from './components/Forms/AddJob';
 import AddPwid from './components/Forms/AddPwid';
+import ResumeTemplate from './components/ResumeTemplate';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <Switch>
           <Route path="/addjob" component={AddJob} />
           <Route path="/addpwid" component={AddPwid} />
+          <Route path="/resumetemplate" component={ResumeTemplate} />
         </Switch>
       </BrowserRouter>
       </div>
---
+++ my-app/src/components/ResumeTemplate.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+import AllInbox from '@material-ui/icons/AllInbox';
+import TagFaces from '@material-ui/icons/TagFaces';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const styles = (theme) => ({
+    root: {
+        width: '100%',
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 3,
+        marginBottom: theme.spacing.unit * 3,
+        padding: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+            marginTop: theme.spacing.unit * 6,
+            marginBottom: theme.spacing.unit * 6,
+            padding: theme.spacing.unit * 3,
+        },
+    },
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 2,
+        marginRight: theme.spacing.unit * 2,
+        [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    icon: {
+        
+    }
+});
+
+class ResumeTemplate extends Component {
+
+    render() {
+        const { classes } = this.props;
+        const name = "Jason";
+        const interest1 = "shopping";
+        const interest2 = "listening to music";
+        const interest3 = "spending time with family";
+
+
+        return (
+            <div className={classes.layout} >
+                <Paper className={classes.paper}>
+                    <Typography component="h2" variant="display2" align="center" style={{ color: '#6969B3' }} gutterBottom >
+                        Hello, my name is {name}.
+                    </Typography>
+                    <br></br>
+                    <Typography component="h2" variant="headline" align="center" style={{ color: '#000' }} gutterBottom >
+                        My personal interests: 
+                    </Typography>
+                    <Typography component="h2" variant="display1" align="center" style={{ color: '#FE5F55' }} gutterBottom >
+                        I love <ShoppingBasket /> shopping <ShoppingBasket /> .
+                    </Typography>
+                    <br></br><br></br>
+                    <Typography component="h2" variant="headline" align="center" style={{ color: '#000' }} gutterBottom >
+                        My skills are: 
+                    </Typography>
+                    <Typography component="h2" variant="display1" align="center" style={{ color: '#44A1A0' }} gutterBottom >
+                        I know how to do <AllInbox /> administrative <AllInbox />.
+                    </Typography>
+                    <br></br><br></br>
+                    <Typography component="h2" variant="headline" align="center" style={{ color: '#000' }} gutterBottom >
+                        My interests are: 
+                    </Typography>
+                    <Typography component="h2" variant="display1" align="center" style={{ color: '#C589E8' }} gutterBottom >
+                        I am interested in <TagFaces /> Hotel <TagFaces />.
+                    </Typography>
+                    <br></br><br></br>
+                    <Typography component="h2" variant="headline" align="center" style={{ color: '#000' }} gutterBottom >
+                    Please give me simple and clear instructions, encouragement and guidance. I may be slow in the beginning but with time, let me show you my capabilities!
+                    </Typography>
+                    <br></br><br></br>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Click here to hire me
+                    </Button>
+                </Paper>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(ResumeTemplate);
